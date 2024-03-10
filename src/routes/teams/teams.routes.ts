74e977f6265c1017a32c1d5bf6e7a61e4e7c76db
hanifa beg <hanifa.beg@gmail.com>
2024-03-10 18:46:41 +0530

    route name change

--- src/routes/teams/teams.routes.ts
+++ src/routes/teams/teams.routes.ts
@@ -4,6 +4,6 @@ import {getMemberbyId,getTeamMembers,getMemberbyName,addTeamMember,removeTeamMem
 // add proper REST endpoints to support Team crud operations
 TeamRouter.get('/teams/getMembers', getTeamMembers)
 TeamRouter.get('/teams/:id', getMemberbyId)
-TeamRouter.get('/teams/:name', getMemberbyName)
+TeamRouter.get('/teams/name/:name', getMemberbyName)
 TeamRouter.post('/teams/addMember', addTeamMember)
 TeamRouter.delete('/teams/:id', removeTeamMember)
